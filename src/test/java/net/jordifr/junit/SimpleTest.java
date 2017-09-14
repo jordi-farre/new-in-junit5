5f43631dd9a7a1483ddb2ca781a80045cf402c8e
Jordi Farré <jordi.farre@mango.com>
2017-09-14 11:34:41 +0200

    repeated test

--- src/test/java/net/jordifr/junit/SimpleTest.java
+++ src/test/java/net/jordifr/junit/SimpleTest.java
@@ -1,6 +1,7 @@
 package net.jordifr.junit;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +12,12 @@ public class SimpleTest {
     @Test
     @DisplayName("This is a simple test method")
     void test() {
+        assertEquals("test", "test");
+    }
+
+    @RepeatedTest(value = 20, name = "{currentRepetition} de {totalRepetitions}")
+    @DisplayName("This is a repeated test")
+    void repeatedTest() {
         assertEquals("test", "test");
     }
 
